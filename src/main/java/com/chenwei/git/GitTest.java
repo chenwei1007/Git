99150c273eb9d352885a3c11a00693242e027d95
chenwei <340359348@qq.com>
2022-04-02 13:28:30 +0800

    测试push

--- src/main/java/com/chenwei/git/GitTest.java
+++ src/main/java/com/chenwei/git/GitTest.java
@@ -7,7 +7,7 @@ public class GitTest {
         System.out.println("hello git3!");
         System.out.println("hello git4 hot-fix!");
         System.out.println("master hot-fix!");
-
         System.out.println("hot-fix!test");
+        System.out.println("push!test");
     }
 }
